437e5a667232521c91229e6e07d0c6c8074f3f7b
Sebastien Requiem <sebastien.requiem@gmail.com>
2014-01-25 18:00:39 +0100

    adds projectile to SGML mode

--- .emacs.d/setup-html-mode.el
+++ .emacs.d/setup-html-mode.el
@@ -1,6 +1,7 @@
 (require 'emmet-mode)
 
 (add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'sgml-mode-hook 'projectile-on)
 
 (defadvice sgml-delete-tag (after reindent-buffer activate)
   (cleanup-buffer))
