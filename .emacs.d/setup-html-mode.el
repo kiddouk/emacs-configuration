4e3fa9b8d8ee201da3a4909b5a665c28602f1135
Sebastien Requiem <sebastien.requiem@gmail.com>
2014-02-22 15:09:56 +0100

    Diable Projectil for HTML files

--- .emacs.d/setup-html-mode.el
+++ .emacs.d/setup-html-mode.el
@@ -1,7 +1,6 @@
 (require 'emmet-mode)
 
 (add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'sgml-mode-hook 'projectile-on)
 
 (defadvice sgml-delete-tag (after reindent-buffer activate)
   (cleanup-buffer))
