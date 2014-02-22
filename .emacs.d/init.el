255da55356717afb03d7f0b749226b3b5b9f0ad0
Sebastien Requiem <sebastien.requiem@gmail.com>
2014-02-22 15:09:11 +0100

    Update ispell program to use

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,7 +79,8 @@
 ;;; Remove the toolbar if compiled for X
 (tool-bar-mode 0)
 
-;;; Define Tabbing 
+;;; Define Tabbing to never use TAB for indent
+;;; and use 4 spaces. ALWAYS.
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
@@ -96,10 +97,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(ispell-program-name "/usr/local/bin/aspell"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
