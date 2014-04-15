caf6bee9449af065bfaa0a62e8067c6dc9208461
Sébastien Requiem <sebastien.requiem@gmail.com>
2014-04-16 00:20:17 +0900

    Update python setup file

--- .emacs.d/setup-python.el
+++ .emacs.d/setup-python.el
@@ -7,9 +7,19 @@
 (defun setup-python-mode ()
   (require 'highlight-indentation)
   (highlight-indentation-mode)
+ 
 
-   (message "test")
-
+  (require 'pymacs)
+  (autoload 'pymacs-apply "pymacs")
+  (autoload 'pymacs-call "pymacs")
+  (autoload 'pymacs-eval "pymacs" nil t)
+  (autoload 'pymacs-exec "pymacs" nil t)
+  (autoload 'pymacs-load "pymacs" nil t)
+  (autoload 'pymacs-autoload "pymacs")
+  ;;(eval-after-load "pymacs"
+  ;;  '(add-to-list 'pymacs-load-path YOUR-PYMACS-DIRECTORY"))
+  (pymacs-load "ropemacs" "rope-")
+  (setq ropemacs-enable-autoimport t)
 
 )
 
