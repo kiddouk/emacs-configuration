b391688cb125e60d6be8061c2bf513b56a9b5b2b
Sebastien Requiem <sebastien.requiem@gmail.com>
2014-10-06 23:57:47 +0300

    conf update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,15 +4,11 @@
 (add-to-list 'load-path "~/.emacs.d/defuns")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/slime")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/swank-js")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/jdee-2.4.1/lisp")
 
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
-;; Uses Marmelade package repository
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
 
 ;; We tune in the Garbage Collector to prevent
 ;; to fire it too often (every ~20MB)
@@ -21,9 +17,6 @@
 ;; Make sure that the .emacs.d is up to date
 ;; This is using Cask (and Pallet) to manage dependencies
 ;; And it should be installed and in the path first
-(require 'cask)
-(cask-initialize)
-
 ;;; Font is probably the most important setting. I want to read code.
 (set-face-attribute 'default nil :family "Source Code Pro")
 (set-face-attribute 'default nil :height 140)
@@ -59,9 +52,11 @@
 (require 'setup-swank)
 (require 'setup-yasnippet)
 (require 'setup-auto-complete)
+(require 'setup-coffee-mode)
+(require 'setup-jdee-mode)
 
 ;;; Load theme
-(load-theme 'solarized-dark)
+;;; (load-theme 'solarized-light)
 
 ;;; Start emacs-server
 (server-start)
@@ -90,15 +85,17 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
+(add-to-list 'auto-mode-alist '("\\.coffee\\'" . coffee-mode))
+(add-to-list 'auto-mode-alist '("\\.java\\'" . jde-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(ispell-program-name "/usr/local/bin/aspell"))
+ '(coffee-tab-width 2)
+ '(ispell-program-name "/usr/local/bin/aspell")
+ '(jde-complete-function (quote jde-complete-menu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
