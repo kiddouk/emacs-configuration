ba702f517ccf891b4094b3486262de140a184a91
Sebastien Requiem <sebastien.requiem@gmail.com>
2015-05-06 09:33:32 +0200

    Remove useless files now

--- setup-coffee-mode.el
+++
@@ -1,7 +0,0 @@
-(require 'coffee-mode)
-
-(add-hook 'sgml-mode-hook 'emmet-mode)
-
-(custom-set-variables '(coffee-tab-width 2))
-
-(provide 'setup-coffee-mode)
--- setup-html-mode.el
+++
@@ -1,8 +0,0 @@
-(require 'emmet-mode)
-
-(add-hook 'sgml-mode-hook 'emmet-mode)
-
-(defadvice sgml-delete-tag (after reindent-buffer activate)
-  (cleanup-buffer))
-
-(provide 'setup-html-mode)
