30a0fbb9bc9fdc804cc373c1f510c8fdd7b6b1a9
Sébastien Requiem <sebastien.requiem@gmail.com>
2015-05-09 18:19:01 +0200

    Set backup directory

--- init.el
+++ init.el
@@ -131,7 +131,22 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 
+;; make backup to a designated dir, mirroring the full path
 
+(defun my-backup-file-name (fpath)
+  "Return a new file path of a given file path.
+If the new path's directories does not exist, create them."
+  (let* (
+        (backupRootDir "~/.emacs.d/emacs-backup/")
+        (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, ⁖ “C:”
+        (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
+        )
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath
+  )
+)
+
+(setq make-backup-file-name-function 'my-backup-file-name)
 
 
 
