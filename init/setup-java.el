3f3685d0ee2f22c24ad6fccccd74451df916a0b4
Sébastien Requiem <sebastien.requiem@gmail.com>
2015-09-19 13:32:07 +0200

    Enable gradle compile + error catch

--- init/setup-java.el
+++ init/setup-java.el
@@ -7,11 +7,34 @@
   (setq jde-complete-function (quote jde-complete-menu))
   (setq jde-global-classpath (quote ("/Users/sebastien/android-sdk/platforms/android-22/android.jar")))
   (setq jde-jdk-registry (quote (("1.8.0" . "/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home"))))
-   (setq jde-sourcepath (quote ("/Users/kiddoun/code/android/voyr")))
+   (setq jde-sourcepath (quote ("/Users/kiddouk/code/android/voyr")))
    (setq jde-jdk (quote ("1.8.0")))
    )
 
+(require 'cl)
+;; Helper function to find files. Source: emacswiki
+(defun* get-closest-pathname (&optional (file "Makefile"))
+  "Determine the pathname of the first instance of FILE starting from the current directory towards root.
+This may not do the correct thing in presence of links. If it does not find FILE, then it shall return 
+the current directory"
+  (let ((root (expand-file-name "/")))
+    (loop for d = default-directory 
+          then (expand-file-name ".." d)
+          if (file-exists-p (expand-file-name file d))  return d
+          if (equal d root) return nil)))
+
+
+(require 'compile)
+
+(defun gradleMake ()
+  (unless (file-exists-p "gradlew")
+    (set (make-local-variable 'compile-command)
+         (let ((mkfile (get-closest-pathname "gradlew")))
+           (if mkfile
+               (progn (format "cd %s; ./gradlew assembleDebug"
+                              mkfile))))))
+  (add-to-list 'compilation-error-regexp-alist '(":compile.*?\\(/.*?\\):\\([0-9]+\\): " 1 2)))
+
+(add-hook 'java-mode-hook 'gradleMake)
 (add-hook 'java-mode-hook 'setup-java-mode)
 (provide 'setup-java)
-
-(setup-java-mode)
