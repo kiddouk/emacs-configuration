2a45e61b52c120dc2e3f272352277a9fe968f720
Sebastien Requiem <sebastien.requiem@gmail.com>
2020-01-07 14:24:45 +0100

    Allow to see each action only for the tap @office

--- init/setup-org.el
+++ init/setup-org.el
@@ -139,6 +139,29 @@
                          "~/notes/tickler.org"))
 
 
+;;; GTD specific
+;;; Allow the agenda to show the next action only for each project
+(setq org-agenda-custom-commands 
+      '(("o" "At the office" tags-todo "@office"
+         ((org-agenda-overriding-header "Office")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))))
+
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+		  
+(defun org-current-is-todo ()
+  (string= "TODO" (org-get-todo-state)))
+
 ;;; Shortcuts
 ;;; C-c k (capture)
 
